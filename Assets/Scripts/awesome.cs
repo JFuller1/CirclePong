222f6e0258919c06234d045a4238c817a6e13fcb
erunions <78222901+erunions@users.noreply.github.com>
2022-09-23 09:34:46 -0500

    i did awesome
    it was so cool

---
+++ Assets/Scripts/awesome.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class awesome : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int awesome = 0;
+        for(int i = 0; i < 10; i++)
+        {
+            awesome++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
