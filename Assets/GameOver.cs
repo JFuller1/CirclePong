24119766b4ad93f158f04c02e1471bec69d20fe0
HeliturtleOP <oproulx8896@gmail.com>
2022-09-28 20:51:21 -0500

    Added lose condition
    you can now lose the game though nothing happens when you do. Basically almost done game, only need sound, main menu, lose screen, and win condition/screen

---
+++ Assets/GameOver.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject egg;
+
+    public GameObject mouth;
+
+    bool dead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (egg != null)
+        {
+            if (dead)
+            {
+                if (Vector3.Magnitude(mouth.transform.position - egg.transform.position) <= 0.4)
+                {
+                    Destroy(egg);
+                    mouth.GetComponent<Animator>().Play("Closed");
+                }
+                else
+                {
+                    mouth.transform.position = Vector3.Lerp(mouth.transform.position, egg.transform.position, Time.deltaTime * 5);
+                }
+            }
+            else
+            {
+                mouth.transform.position = new Vector2(egg.transform.position.x, mouth.transform.position.y);
+            }
+        }
+        else
+        {
+            Vector3 offScreen = new Vector2(mouth.transform.position.x, -12);
+            mouth.transform.position = Vector3.Lerp(mouth.transform.position, offScreen, Time.deltaTime * 2);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("t");
+        dead = true;
+        egg.GetComponent<ballController>().ballSpeed = 0.1f;
+    }
+}
